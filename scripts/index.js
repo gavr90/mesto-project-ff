be6d9247b387561f10721486d16d796fbd088f36
Sergei Gavrilov <iamgavr@yandex.ru>
2023-12-22 00:40:33 +0300

    удаление карточки вынесено в отдельную функцию

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,25 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard(cardImage, cardTitle) {
+function addCard(cardImage, cardTitle, deleteCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = cardImage;
   cardElement.querySelector('.card__title').textContent = cardTitle;
-  const button = cardElement.querySelector('.card__delete-button')
-  button.addEventListener('click', function () {
-    const card = button.closest('.card');
-    card.remove();
-  });
+  const buttonDelete = cardElement.querySelector('.card__delete-button');
+  buttonDelete.addEventListener('click', () => deleteCard(buttonDelete));
   return cardElement;
 };
 
 // @todo: Функция удаления карточки
-
+function deleteCard(buttonDelete) {
+  const card = buttonDelete.closest('.card');
+  card.remove(); 
+}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (element) {
   let cardImage = element.link;
   let cardTitle = element.name;
-  placesList.append(addCard(cardImage, cardTitle));
+  placesList.append(addCard(cardImage, cardTitle, deleteCard));
 })
 
