a68bd5321112742b5e1fceb0b5c80971948b3f66
Sergei Gavrilov <iamgavr@yandex.ru>
2023-12-23 15:30:41 +0300

    добавлен подбор значений к атрибуту alt у картинок в карточках

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,24 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function addCard(cardImage, cardTitle, deleteCard) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = cardImage;
-  cardElement.querySelector('.card__title').textContent = cardTitle;
-  const buttonDelete = cardElement.querySelector('.card__delete-button');
-  buttonDelete.addEventListener('click', () => deleteCard(buttonDelete));
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__image").alt = 'фото ' + cardTitle;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
+  buttonDelete.addEventListener("click", () => deleteCard(buttonDelete));
   return cardElement;
-};
+}
 
 // @todo: Функция удаления карточки
 function deleteCard(buttonDelete) {
-  const card = buttonDelete.closest('.card');
-  card.remove(); 
+  const card = buttonDelete.closest(".card");
+  card.remove();
 }
 
 // @todo: Вывести карточки на страницу
@@ -25,5 +26,4 @@ initialCards.forEach(function (element) {
   let cardImage = element.link;
   let cardTitle = element.name;
   placesList.append(addCard(cardImage, cardTitle, deleteCard));
-})
-
+});
